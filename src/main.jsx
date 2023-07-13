4242d1d86443e4650689668d275ecf3653b7c18f
RodrianVANDERSMIT <rodrian33@gmail.com>
2023-07-13 10:31:33 +0200

    Setup MUI CssBaseline (equivalent of normalize css) and Roboto font

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import CssBaseline from '@mui/material/CssBaseline'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.Fragment>
 
-    <App />
+        <CssBaseline />
+        <App />
 
+    </React.Fragment>
+    
 )
