ac110d64c08b332f5c5e9cd6bc03dab2589452b7
RodrianVANDERSMIT <rodrian33@gmail.com>
2023-07-13 10:41:45 +0200

    Setup minimal BEMIT boilerplate and the components folder

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+
+
+import App from './components/App/App'
 import CssBaseline from '@mui/material/CssBaseline'
 import '@fontsource/roboto/400.css'
 import '@fontsource/roboto/500.css'
