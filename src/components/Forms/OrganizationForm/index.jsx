39cb8bc4d71779161ae227f3afabd8a4242a5db0
RodrianVANDERSMIT <rodrian33@gmail.com>
2023-07-17 16:19:38 +0200

    Add organization form and set up a new toolkit architecture named organization

--- src/components/Forms/OrganizationForm/index.jsx
+++ src/components/Forms/OrganizationForm/index.jsx
@@ -1,10 +1,34 @@
+import { TextField, Button } from '@mui/material'
+import { useForm } from 'react-hook-form'
+import {useDispatch} from 'react-redux'
 import './style.scss'
+import { createOrganization } from '../../../redux/reducers/organization'
 
 function OrganizationForm() {
 
+const {register, handleSubmit} = useForm();
+const dispatch = useDispatch();
+
+
+const onSubmit = (organization) =>{
+    console.log(organization);
+    dispatch(createOrganization(organization))
+}
+
+
     return (
-        <div>
-            Ici le formulaire de lorganisation
+        <div className="c-organization-form">
+            <p className="c-organization-form__text">Bienvenue sur la page de création d'une organisation, merci de bien vouloir renseignez le nom de votre organisation et valider</p>
+            <form className="c-organization-form__form" onSubmit={handleSubmit(onSubmit)}>
+                <TextField 
+                name="organization"
+                type="text"
+                label="Nom de votre organisation"
+                {...register('organization',{required:true})}
+                />
+                <Button sx={{ m:1,}} className="c-organization-form__button" variant="contained" type="submit" >Valider</Button>
+            </form>
+            
         </div>
     )
 }
