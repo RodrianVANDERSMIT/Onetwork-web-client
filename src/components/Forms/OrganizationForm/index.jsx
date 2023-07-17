bfdf21a9fdf81dab7873b20ce34b414e1e43c37c
RodrianVANDERSMIT <rodrian33@gmail.com>
2023-07-17 18:16:06 +0200

    creation of the organization creation page

--- src/components/Forms/OrganizationForm/index.jsx
+++ src/components/Forms/OrganizationForm/index.jsx
@@ -2,7 +2,7 @@ import { TextField, Button } from '@mui/material'
 import { useForm } from 'react-hook-form'
 import {useDispatch} from 'react-redux'
 import './style.scss'
-import { createOrganization } from '../../../redux/reducers/organization'
+import { newOrganization } from '../../../redux/reducers/organization'
 
 function OrganizationForm() {
 
@@ -10,25 +10,25 @@ const {register, handleSubmit} = useForm();
 const dispatch = useDispatch();
 
 
-const onSubmit = (organization) =>{
-    console.log(organization);
-    dispatch(createOrganization(organization))
+const onSubmit = ({organizationName}) =>{
+    console.log(organizationName);
+    dispatch(newOrganization(organizationName))
 }
 
 
     return (
         <div className="c-organization-form">
-            <p className="c-organization-form__text">Bienvenue sur la page de création d'une organisation, merci de bien vouloir renseignez le nom de votre organisation et valider</p>
+            <h2>Bienvenue sur la page de création d'une organisation !</h2>
+            <p className="c-organization-form__text">Merci de bien vouloir renseigner le nom de votre organisation et cliquer sur le bouton de validation pour continuer.</p>
             <form className="c-organization-form__form" onSubmit={handleSubmit(onSubmit)}>
                 <TextField 
                 name="organization"
                 type="text"
                 label="Nom de votre organisation"
-                {...register('organization',{required:true})}
+                {...register('organizationName',{required:true})}
                 />
                 <Button sx={{ m:1,}} className="c-organization-form__button" variant="contained" type="submit" >Valider</Button>
             </form>
-            
         </div>
     )
 }
