327b85e8aa66ae4b257227faf0461bb6895ee817
RodrianVANDERSMIT <rodrian33@gmail.com>
2023-07-18 09:29:10 +0200

    add the validate on the form

--- src/components/Forms/OrganizationForm/index.jsx
+++ src/components/Forms/OrganizationForm/index.jsx
@@ -6,7 +6,7 @@ import { newOrganization } from '../../../redux/reducers/organization'
 
 function OrganizationForm() {
 
-const {register, handleSubmit} = useForm();
+const { register, handleSubmit, formState: { errors } } = useForm()
 const dispatch = useDispatch();
 
 
@@ -16,21 +16,24 @@ const onSubmit = ({organizationName}) =>{
 }
 
 
-    return (
-        <div className="c-organization-form">
-            <h2>Bienvenue sur la page de création d'une organisation !</h2>
-            <p className="c-organization-form__text">Merci de bien vouloir renseigner le nom de votre organisation et cliquer sur le bouton de validation pour continuer.</p>
-            <form className="c-organization-form__form" onSubmit={handleSubmit(onSubmit)}>
-                <TextField 
-                name="organization"
-                type="text"
-                label="Nom de votre organisation"
-                {...register('organizationName',{required:true})}
-                />
-                <Button sx={{ m:1,}} className="c-organization-form__button" variant="contained" type="submit" >Valider</Button>
-            </form>
-        </div>
-    )
+return (
+    <div className="c-organization-form">
+      <h2>Bienvenue sur la page de création d'une organisation !</h2>
+      <p className="c-organization-form__text">Merci de bien vouloir renseigner le nom de votre organisation et cliquer sur le bouton de validation pour continuer.</p>
+      <form className="c-organization-form__form" onSubmit={handleSubmit(onSubmit)}>
+        <TextField 
+          name="organization"
+          type="text"
+          label="Nom de votre organisation"
+          {...register('organizationName',{required:true, minLength: 3 })}
+        />
+        {errors.organizationName && errors.organizationName.type === "minLength" && (
+          <p className="c-organization-form__error">Le nom de l'organisation doit comporter au moins 3 caractères.</p>
+        )}
+        <Button sx={{ m:1,}} className="c-organization-form__button" variant="contained" type="submit" >Valider</Button>
+      </form>
+    </div>
+  )
 }
 
 export default OrganizationForm
