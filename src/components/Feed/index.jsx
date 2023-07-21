6ce2754702098685fd92d5bdeb85188370046949
Neoxgen <provost.bruno0073@gmail.com>
2023-07-21 12:01:38 +0200

    main/header feed

--- src/components/Feed/index.jsx
+++ src/components/Feed/index.jsx
@@ -1,15 +1,37 @@
-import CardPost from "../Post"
-import PostForm from "../Forms/PostForm"
+import {  Typography,Paper, InputBase } from '@mui/material'
+import { Box, Avatar, IconButton } from '@mui/material'
+
+import HistoryEduIcon from '@mui/icons-material/HistoryEdu';
 
 import './style.scss'
 
 function Feed() {
 
     return (
-        <div> PLACER LE TITRE DE LORGANISATION OU LE PROFIL DE Lutilisateur choisis selon la page .... et mettre en condition  le form post
-            <PostForm />
-            <CardPost />
-        </div>
+        <Box className="c-container-news" >
+			<Box className="c-header-news">
+				<Typography variant="h5" >
+					{"Nom de l'organisation"}
+				</Typography>
+				<Box className="c-header-news__textarea" >
+				<Avatar className="c-avatar" alt="Remy Sharp" src='https://randomuser.me/api/portraits/women/40.jpg' />
+				<Paper
+					component="form"
+					sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 400,marginLeft: '1em' }}
+					>
+					<InputBase
+						sx={{ ml: 1, flex: 1 }}
+						placeholder="Nouveau Post..."
+						inputProps={{ 'aria-label': 'search google maps' }}
+						multiline
+					/>
+					<IconButton type="button" sx={{ p: '10px' }} aria-label="search">
+						<HistoryEduIcon />
+					</IconButton>
+				</Paper>
+				</Box>
+			</Box>
+        </Box>
     )
 }
 
