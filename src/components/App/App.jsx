b4081e6474257eefbe53261bd7d338a5d955251e
Jon Garbayo <jon.garbayo@gmail.com>
2023-09-07 13:05:21 +0200

    ⚰️ Remove the useless PropTypes definition of the App component
    It doesn't accept any props, so this check was not needed.

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,15 +1,9 @@
-import PropTypes from 'prop-types'
 import Router from '../Router'
 
 function App() {
     return (
         <Router/>
     )
-}
-
-
-App.propTypes = {
-    children: PropTypes.node,
 }
 
 export default App
