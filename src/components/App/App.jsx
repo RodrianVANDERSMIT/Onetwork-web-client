88fe0c6661cb3aa9604874efc9055be3b35323d4
Jon Garbayo <jon.garbayo@gmail.com>
2023-09-07 13:04:05 +0200

    ♻️ Move the routes definition in a separate component
    The App component was a little bit too bloated with all those router related code.

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,74 +1,9 @@
-import { Routes, Route } from 'react-router-dom'
 import PropTypes from 'prop-types'
-import ProtectedRoute from '../Router/ProtectedRoute'
-import AdminRoute from '../Router/AdminRoute'
-import UserProfile from "../../views/UserProfile"
-import Home from '../../views/Home'
-import OrganizationCreation from '../../views/OrganizationCreation'
-import Administration from '../../views/Administration'
-import ProfileSettings from '../../views/ProfileSettings'
-import Contact from '../../views/Contact'
-import SignUp from '../../views/SignUp'
-import ActivityFeed from '../../views/ActivityFeed'
-import Error401 from '../../views/Error401'
-import Error403 from '../../views/Error403'
-import Error404 from '../../views/Error404'
-import Error500 from '../../views/Error500'
-
-
-
+import Router from '../Router'
 
 function App() {
     return (
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/new-organization" element={<OrganizationCreation />} />
-            <Route path="/sign-up" element={<SignUp />} />
-            <Route path="/about" element={<Contact />} />
-            <Route path="/error/401" element={<Error401 />} />
-            <Route path="/error/403" element={<Error403 />} />
-            <Route path="/error/404" element={<Error404 />} />
-            <Route path="/error/500" element={<Error500 />} />
-            <Route
-                path={`/:organizationId`}
-                element={
-                    <ProtectedRoute >
-                        <ActivityFeed />
-                    </ProtectedRoute>
-                }
-            />
-            <Route
-                path={`/:organizationId/user/:userId`}
-                element={
-                    <ProtectedRoute >
-                        <UserProfile />
-                    </ProtectedRoute>
-                }
-            />
-            
-            <Route
-                path={`/:organizationId/user/:userId/edit`}
-                element={
-                    <ProtectedRoute >
-                        <ProfileSettings />
-                    </ProtectedRoute>
-                }
-            />
-            <Route
-                path={`/:organizationId/admin/members`}
-                element={
-                    <ProtectedRoute >
-                        <AdminRoute>
-                            <Administration />
-                        </AdminRoute>
-                    </ProtectedRoute>
-                }
-            />
-            <Route 
-                path="/*" 
-                element={ <Error404 />}
-            />
-        </Routes>  
+        <Router/>
     )
 }
 
