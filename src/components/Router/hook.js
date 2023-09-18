1323b7bca42f4ca74888c66bc7299ab0951530a6
Jon Garbayo <jon.garbayo@gmail.com>
2023-09-18 16:39:24 +0200

    🔒️ Add an auto clear of the user state when receiving a 401 error
    It's nedeed when the session expires or when the user account has been disabled while the app was still opened.

--- src/components/Router/hook.js
+++ src/components/Router/hook.js
@@ -1,8 +1,11 @@
 import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 import { api } from '../../services/api'
+import { cleanUserState } from '../../redux/reducers/user'
 
 export default function useInterceptors() {
+    const dispatch = useDispatch()
     const navigate = useNavigate()
 
     useEffect(() => {
@@ -13,8 +16,10 @@ export default function useInterceptors() {
             // When a session expires, or the user logged out itself in another
             // tab than the current one, or his/her account has been disabled
             // while using the app... or a lot of possible scenarios, the next
-            // request will send back a 401 error.
+            // request will send back a 401 error. When it happens, we clean the
+            // user state and redirect to the home page.
             if (error.response.status === 401) {
+                dispatch(cleanUserState())
                 navigate('/')
             }
 
