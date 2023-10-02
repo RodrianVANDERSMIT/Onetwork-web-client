bf3f91e75658f71c5203bb26b84cf3f230d95d97
Jon Garbayo <jon.garbayo@gmail.com>
2023-10-02 17:34:53 +0200

    👽️ Update the API base URL due to a change server-side
    The /api prefix has been removed to simplify the use of the API, as there it will never return any page as response. Thanks to this, the call to the CSRF cookie generation endpoint will be easier because it doesn't have this prefix by default.

--- src/services/api.js
+++ src/services/api.js
@@ -2,6 +2,6 @@ import axios from "axios"
 
 
 export const api = axios.create({
-    baseURL: `${import.meta.env.VITE_API_ORIGIN}/api`,
+    baseURL: `${import.meta.env.VITE_API_ORIGIN}`,
     withCredentials: true
 })
