d737b507520b5a39526830cf8f272429cfa07ac1
Jon Garbayo <jon.garbayo@gmail.com>
2023-10-03 16:36:46 +0200

    🧑‍💻 Add a function to easily fetch the current CSRF cookie

--- src/services/api.js
+++ src/services/api.js
@@ -5,3 +5,11 @@ export const api = axios.create({
     baseURL: `${import.meta.env.VITE_API_ORIGIN}`,
     withCredentials: true
 })
+
+/**
+ * Fetch the CSRF cookie from the API. Must be called before each form
+ * submission.
+ */
+export const fetchCsrfCookie = async () => {
+    await api('/sanctum/csrf-cookie')
+}
