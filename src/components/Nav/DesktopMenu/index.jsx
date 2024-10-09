2f73c420c734e699129730f5cce5c2cfae47b6e0
Jon Garbayo <jon.garbayo@gmail.com>
2024-10-09 13:20:06 +0200

    🚸 Remove the link to the user's profile in the main menu
    Two reasons:
    - the label "Mon profil" was not written in the same person as the new titles on the page ("Votre profil" or "Créez votre profil"), which felt strange
    - there was already a link to the user's profile on their name and avatar just above the menu, as seen on other social networks like Facebook or Instagram

--- src/components/Nav/DesktopMenu/index.jsx
+++ src/components/Nav/DesktopMenu/index.jsx
@@ -28,7 +28,6 @@ const DesktopMenu = () => {
 
     const data = [
         { text: "Flux d'activité", icon: <ForumIcon />, route: `/${organizationId}`, show: true },
-        { text: 'Mon profil', icon: <PersonIcon/>, route: `/${organizationId}/user/${userId}`, show: true},
         { text: 'Editer mon profil', icon: <ManageAccountsIcon/>, route: `/${organizationId}/user/${userId}/edit`, show: true},
         { text: 'Administration', icon: <AdminPanelSettingsIcon />, route: `/${organizationId}/admin/members`, show: isAdmin },
         { text: 'Contact', icon: <ContactMailIcon />, route: "/about", show: true },
