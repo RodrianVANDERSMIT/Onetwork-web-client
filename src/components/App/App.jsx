69c1f18bf9640a548cd07540b92a31ca0509130d
Jon Garbayo <jon.garbayo@gmail.com>
2024-10-18 23:07:50 +0200

    🔒️ Removed the user data from localStorage
    These data was stored in localStorage to persist the user between page reloads and after closing the tab or the browser, as long as the session was still active server-side. However, this system was only temporary due to a serious security issue. In fact, localStorage data can easily be modified by the end user, and nothing had been implemented to check the validity of the data.
    Now, the current user is simply fetched from the server before doing anything else, preventing the security issues mentioned above, and also ensuring the user data is up to date if it was modified on another device or browser.

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,8 +1,23 @@
+import { useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { fetchUser } from '../../redux/thunks/user'
 import Router from '../Router'
 
 function App() {
+    const dispatch = useDispatch()
+    const [isFetchingUser, setIsFetchingUser] = useState(true)
+
+    useEffect(() => {
+        (async () => {
+            await dispatch(fetchUser())
+            setIsFetchingUser(false)
+        })()
+    }, [dispatch])
+
     return (
-        <Router/>
+        isFetchingUser ?
+            "" :
+            <Router/>
     )
 }
 
