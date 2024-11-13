da6128165249d58e0c491811a6bf4b8e52832033
Jon Garbayo <jon.garbayo@gmail.com>
2024-11-13 11:41:09 +0100

    ⚰️ Remove the routes to the error pages, now unnecessary

--- src/components/Router/index.jsx
+++ src/components/Router/index.jsx
@@ -10,10 +10,7 @@ import ProfileSettings from '../../views/ProfileSettings'
 import Contact from '../../views/Contact'
 import SignUp from '../../views/SignUp'
 import ActivityFeed from '../../views/ActivityFeed'
-import Error401 from '../../views/Error401'
-import Error403 from '../../views/Error403'
 import Error404 from '../../views/Error404'
-import Error500 from '../../views/Error500'
 import useInterceptors from './hook'
 
 export default function Router() {
@@ -34,10 +31,6 @@ export default function Router() {
                 </GuestRoute>
             } />
             <Route path="/about" element={<Contact />} />
-            <Route path="/error/401" element={<Error401 />} />
-            <Route path="/error/403" element={<Error403 />} />
-            <Route path="/error/404" element={<Error404 />} />
-            <Route path="/error/500" element={<Error500 />} />
             <Route
                 path={`/:organizationId`}
                 element={
